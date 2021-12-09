d85a79ca8124f1280170564caef702c89145b05a
shuhelahm <shuhel037@gmail.com>
2021-12-09 20:50:02 +0600

    header redesigned

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Tech School Draft</h1>
-            <h3>Plan your school tech committee</h3>
-            <h2>Total Budget: $1100</h2>
+            <h2>Tech School Draft</h2>
+            <p>Plan your school tech committee</p>
+            <p>Total Budget: $1100</p>
         </div>
     );
 };
